4003bc2c7a81c050c06187a7755c061e7294ad67
Marcus Agbaglah <magbaglah360@gmail.com>
2022-09-06 11:20:13 +0100

    routes compelted

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :ingredients, only: [:index]
+  resources :cocktails, only: [:index, :show]
+  resources :user_ingredients, only: [:index] do
+    resources :ingredients, only: [:create, :new, :destroy]
+  end
 end
