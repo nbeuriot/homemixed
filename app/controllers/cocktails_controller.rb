b5d14439ab7cdba3b10a5eb63fe07181da613b6f
Nicolas BEURIOT <nbeuriot@gmail.com>
2022-09-12 11:38:43 +0200

    Select Cocktail logic work

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,10 +9,38 @@ class CocktailsController < ApplicationController
 
   def ready_to_make
     # @cocktails = Cocktail.first(5)
-    @cocktails = Cocktail.where(name: "Mojito")
+    # @cocktails = Cocktail.where(name: "Mojito")
+    puts "=================================================="
+    puts "|                Cocktail Finder                 |"
+    puts "=================================================="
+    puts " "
+    result = []
+    users_ingredients = current_user.ingredients
+    cocktails = Cocktail.all
+
+    cocktails.each do |cocktail|
+      puts " +++++ Looking for #{cocktail.name} ++++++"
+      count = 0
+      cocktail.ingredients.each do |cocktail_ing|
+        users_ingredients.each do |user_ing|
+          if cocktail_ing == user_ing
+            puts "       -- You have #{cocktail_ing.name}"
+            count += + 1
+          end
+        end
+      end
+      result << cocktail if cocktail.ingredients.size <= count
+      puts " "
+      puts " "
+    end
+    puts "**** So you can make ****"
+    result.each do |c|
+      puts "  -- #{c.name}"
+    end
+    @cocktails = result
   end
 
   def show
-    @cocktail = Cocktail.where(name: "Mojito")[0]
+    @cocktail = Cocktail.find(params[:id])
   end
 end
